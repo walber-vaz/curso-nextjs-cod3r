346c83d96537ed3ad13322dce71e50c06770b8cc
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-05-14 21:25:31 -0300

    feat: component jsx in react

---
+++ exercicios/pages/basic/jsx2.jsx
@@ -0,0 +1,10 @@
+export default function jsx2() {
+  // e possível retornar conteúdo jsx
+  const conteudo = (
+    <div>
+      <h1>JSX #02</h1>
+    </div>
+  );
+
+  return conteudo;
+}
--- exercicios/pages/basic/second.jsx
+++ exercicios/pages/basic/second.jsx
@@ -1,7 +1,7 @@
 export default function Second() {
   return (
     <div>
-      <h1>Second</h1>
+      <h1>Second - JSX</h1>
     </div>
   )
 }
